bdbf57ee3ae031e097a445045109d16a1e320a87
Lohan <lohanbruno@hotmail.com>
2016-04-20 04:34:04 +0000

    agora vai

---
+++ db/migrate/20160420041133_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :nome
+      t.string :email
+      t.integer :telefone
+      t.integer :matricula
+      t.integer :cpf
+
+      t.timestamps null: false
+    end
+  end
+end
